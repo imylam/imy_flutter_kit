8b282250e644862db675d3ee4de0402235954b76
imylam <imylamdev@gmail.com>
2019-09-16 00:00:30 +0800

    Add logos assets

--- imy_ui_wigets/test/buttons_test.dart
+++ imy_ui_wigets/test/buttons_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:imy_flutter_ui/imy_ui_wigets.dart';
+import 'package:imy_ui_wigets/imy_ui_wigets.dart';
 
 void main() {
   testWidgets('Check supplied text is used', (WidgetTester tester) async {
